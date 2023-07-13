eb44a11867402e517ab87fc3c266d9b1656a04b2
ragFurlan <robertaalethea@outlook.com>
2023-07-13 17:39:33 -0300

    comite

--- Program.cs
+++ Program.cs
@@ -2,23 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 
 namespace DroneDeliveryService
 {
     internal class Program
     {
         static void Main(string[] args)
-        {          
+        {
             Console.WriteLine("Enter drone information:");
             string[] droneInfo = Console.ReadLine().Split(',');
 
             List<Drone> drones = CreateDrones(droneInfo);
-           
+
             Console.WriteLine("Enter locations and package weights (one per line). If you want to finish inserting the locations press enter again and the program will distribute the locations by the indicated drones:");
             List<Location> locations = ReadLocations();
-           
+
             AssignLocationsToDrones(locations, drones);
-           
+
             PrintDeliveryPlan(drones);
 
             Console.WriteLine("Press any key to end execution");
@@ -59,43 +60,70 @@ namespace DroneDeliveryService
 
         static void AssignLocationsToDrones(List<Location> locations, List<Drone> drones)
         {
-           locations = locations.OrderBy(x => x.PackageWeight).ToList();
-           drones = drones.OrderByDescending(x => x.MaxWeight).ToList();
-                       
+            locations = locations.OrderByDescending(x => x.PackageWeight).ToList();
+            drones = drones.OrderByDescending(x => x.MaxWeight).ToList();
+
             while (locations.Count > 0)
             {
-                int locationsCount = SearchLocations(locations, drones);
-                locations = locations.Skip(locationsCount).ToList();               
+                locations = SearchLocations(locations, drones);
             }
 
         }
 
-        static int SearchLocations(List<Location> locations, List<Drone> drones)
+        static List<Location> SearchLocations(List<Location> locations, List<Drone> drones)
         {
-            int locationsCountAll = 0;
+            List<Location> locationsCountAll = locations;
             foreach (Drone drone in drones)
             {
-                int locationsCountPerDrone = 0;
-                for (int i = 0; i < locations.Count; i++)
-                {
-                    if (drone.RemainingCapacity >= locations[i].PackageWeight)
-                    {
-                        drone.AddLocation(locations[i]);
-                        locationsCountPerDrone++;
-                        locationsCountAll++;
-                    }
-                    if (drone.RemainingCapacity == 0) 
-                    {                     
-                        break;
-                    }                   
-                }
-
-                drone.ResetRemainingCapacityToNextDrone();
-                locations = locations.Skip(locationsCountPerDrone).ToList();
+                locations = SearchLocations2(locations, drone, locationsCountAll);
 
             }
+            return locations;
+        }
+
+        static List<Location> SearchLocations2(List<Location> locations, Drone drone, List<Location> locationsCountAll)
+        {
+            List<Location> locationForTrip = new List<Location>();
+
+            while (drone.RemainingCapacity == drone.MaxWeight && locationsCountAll.Count > 0)
+            {
+                locationsCountAll = SearchLocations3(locations, drone, locationsCountAll);
+            }
+
+            return locationsCountAll;
+
+        }
+
+        static List<Location> SearchLocations3(List<Location> locations, Drone drone, List<Location> locationsCountAll)
+        {
+            List<Location> locationForTrip = new List<Location>();
+            for (int l = 0; l < locations.Count; l++)
+            {
+                if (drone.RemainingCapacity >= locations[l].PackageWeight)
+                {
+                    locationForTrip.Add(locations[l]);
+                    drone.AddLocation(locations[l]);
+                }
+                if (drone.RemainingCapacity == 0)
+                {
+                    break;
+                }
+            }
+
+            foreach (var location in locationForTrip)
+            {
+                locationsCountAll.Remove(location);
+               
+            }
+
+            if (drone.RemainingCapacity == 0)
+            {
+                drone.ResetRemainingCapacityToNextDrone();
+            }
+
             return locationsCountAll;
         }
+
 
         static void PrintDeliveryPlan(List<Drone> drones)
         {
